8b3dfda51f70cf7df1ecf3f3b6e0b623e478eefd
FRobotics Team 4150 <froboticsteam4150@gmail.com>
2017-11-04 16:01:49 -0400

    [2017-11-04 04:01 PM] test reason

--- push.bat
+++ push.bat
@@ -2,6 +2,7 @@
 git pull origin master
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
-git commit -am "%mydate% %mytime%"
+set /p reason=
+git commit -am "[%mydate% %mytime%] %reason%"
 git push origin master
 pause
